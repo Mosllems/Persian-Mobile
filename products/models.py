4d7770789912ff5b0aba0a9d0d1b1683b2981013
moslem <moslemamiri82@gmail.com>
2025-05-07 20:18:06 +0330

    add translation

--- products/models.py
+++ products/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class Product(models.Model):
     FARSI_MENU = {
-        ("D", "Darad"),
-        ("N", "Nadarad"),
+        ("D", _("Darad")),
+        ("N", _("Nadarad")),
 
     }
     name = models.CharField(max_length=1000, default="lorem ipsum")
